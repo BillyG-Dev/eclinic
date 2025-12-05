a4844f4aa55099119fbb1255bd30183e9e673d3a
Billy <billcheruiyotyegon@gmail.com>
2025-12-05 08:58:51 +0300

    error

--- eclinic/accounts/urls.py
+++ eclinic/accounts/urls.py
@@ -5,10 +5,4 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-
-    path('dashboard/', views.dashboard_redirect, name='dashboard_redirect'),
-    
-    path('dashboard/patient/', views.patient_dashboard_view, name='patient_dashboard'),
-    path('dashboard/clinician/', views.clinician_dashboard_view, name='clinician_dashboard'),
-    path('dashboard/admin/', views.admin_dashboard_view, name='admin_dashboard'),
-]
+    ]
